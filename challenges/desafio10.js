b0c784506055672cb1d8c4e74bc8e5005827f768
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-09-19 16:22:28 -0300

    task 10 fix 1.1

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,14 +1,16 @@
 db.produtos.updateMany({},
-    { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] },
+    { $push: { vendasPorDia: { $each: [0, 0, 0, 0, 0, 0, 0] } },
 });
 
-db.produtos.updateMany({},
-    { nome: "Big Mac" }, { $inc: { "vendasPorDia.3": +60 },
-});
+db.produtos.updateMany(
+    { nome: "Big Mac" },
+    { $inc: { "vendasPorDia.3": 60 } },
+);
 
-db.produtos.updateMany({ tags: { $all: ["bovino", "pão"] } },
-    { $inc: { "vendasPorDia.6": +120 },
-});
+db.produtos.updateMany(
+    { tags: ["bovino", "pão"] },
+    { $inc: { "vendasPorDia.6": 120 } },
+);
 
 db.produtos.find({},
     { _id: false, nome: true, vendasPorDia: true,
