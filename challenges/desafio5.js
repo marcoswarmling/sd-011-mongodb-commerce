4ee3573fe1f8911f5a5470ae871c8f89b568844a
Marconi <marconieng@gmail.com>
2021-09-19 19:32:51 -0300

    Corrigindo desafio 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany(
   { nome: { $ne: "McChicken" } },
-  { $push: { ingredientes: "ketchup" } },
+  { $push: { $addToSet: { ingredientes: "ketchup" } } }
 );
 
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
