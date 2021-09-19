2e5921add72e3b922418465d7659a0a5b5ea250c
Marconi <marconieng@gmail.com>
2021-09-19 20:21:01 -0300

    Corrigindo desafio 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany(
   { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
-  { $push: { ingredientes: "bacon", $position: -1 } },
+  { $push: { ingredientes: "bacon" } },
 );
 
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
