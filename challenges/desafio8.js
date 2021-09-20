9cdd39447969ab128a0c41bb9959f9d3d2d18a41
Tulio Sploradori <tuliospl@gmail.com>
2021-09-20 18:49:31 -0300

    Lint

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,5 +1,9 @@
 db.getSiblingDB("commerce").produtos.updateOne(
   { nome: "Quarteirão com Queijo" },
-  { $pop: { ingredientes: -1 } });
+  { $pop: { ingredientes: -1 } },
+);
 
-db.getSiblingDB("commerce").produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
+db.getSiblingDB("commerce").produtos.find(
+  {},
+   { _id: 0, nome: 1, ingredientes: 1 },
+);
