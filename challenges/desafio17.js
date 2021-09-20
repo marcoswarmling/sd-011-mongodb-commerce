9ab5ef65deced8785890625fcb2179caf8be681f
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-09-20 00:13:36 -0300

    feito 17 e 18

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,1 +1,5 @@
-db.commerce.count({ $text: { $search: "frango hamburguer" } })
+db.produtos.createIndex(
+  { descricao: "text" },
+  { default_language: "portuguese" },
+);
+db.produtos.count({ $text: { $search: "frango hamburguer" } })
