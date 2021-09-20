21bf5b3d7eea2ff66607e373936627aef1dc6039
theusseveen22 <mateus.krauss@gmail.com>
2021-09-20 20:27:19 -0300

    Adiciona requisitos do 11 acima

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -3,7 +3,8 @@ db.produtos.updateMany(
     { $push:
         { tags: {
             $each: ["combo", "tasty"],
-            $sort: { tags: 1 } },
+            $sort: 1,
+        },
         },
      },
 );
