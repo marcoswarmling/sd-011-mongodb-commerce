feedb9101275d576e776b9e9dfc9cfde9434bba5
Tulio Sploradori <tuliospl@gmail.com>
2021-09-20 16:10:19 -0300

    Req 1 ao 10

--- .github/challenges/desafio1.js
+++ .github/challenges/desafio1.js
@@ -4,4 +4,3 @@ $set: { criadoPor: "Ronald McDonald" }});
 
 db.produtos
 .find({}, { _id: 0, nome: 1, criadoPor: 1 });
-  
--- .github/challenges/desafio7.js
+++ .github/challenges/desafio7.js
@@ -5,4 +5,3 @@ db.produtos
 
 db.produtos
 .find({}, { _id: 0, nome: 1, ingredientes: 1 });
-  
