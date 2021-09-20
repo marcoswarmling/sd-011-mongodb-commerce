cd5c39574c454844570098e1a89f21fd3e5d8ad0
samuelvanguarda <samuelvanguarda@gmail.com>
2021-09-20 19:19:46 -0300

    Correção - Req 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,5 +1,5 @@
-db.produtos.updateMany({ $and: [{ nome: "Quarteirao com Queijo" }, { nome: "Big Mac" }] },
-  { $push: { ingredientes: "bacon" } });
+db.produtos.updateMany({ nome: { $in: ["Quarteirao com Queijo", "Big Mac"] } },
+{ $push: { ingredientes: "bacon" } });
 
 db.produtos.find({}, 
 { _id: 0, nome: 1, ingredientes: 1 });
