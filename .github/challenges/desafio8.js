fb1c1ec59608fef201cbba32dcc305e8aea102ef
Tulio Sploradori <tuliospl@gmail.com>
2021-09-20 15:54:13 -0300

    Req 1 ao 10

---
+++ .github/challenges/desafio8.js
@@ -0,0 +1,7 @@
+db.getSiblingDB("commerce").produtos
+.updateOne(
+  { nome: "Quarteirão com Queijo" },
+  { $pop: { ingredientes: -1 } });
+
+db.getSiblingDB("commerce").produtos
+.find({}, { _id: 0, nome: 1, ingredientes: 1 });
