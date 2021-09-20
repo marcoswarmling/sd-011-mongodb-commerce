e85d3284e539ef7f5dd5fb3e047fb9ca08d69c25
Gustavo Lemes <gustavolemes997@gmail.com>
2021-09-19 21:44:23 -0300

    Requisito 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,16 @@
+db.produtos.updateMany(
+  {
+    nome: { $ne: "McChicken" },
+    ingredientes: { $not: { $elemMatch: { $eq: "ketchup" } } },
+  },
+   { $push: { ingredientes: "ketchup" } },
+);
+
+db.produtos.find(
+  { },
+  { 
+    nome: 1,
+    ingredientes: 1,
+    _id: 0,
+  },
+);
