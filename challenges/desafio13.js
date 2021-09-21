1be6646581dad9b9a86137dd7287e22b2ff562c3
DADA <davimarque2000@gmail.com>
2021-09-21 16:02:46 -0300

    desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,11 @@
+db.produtos.updateMany(
+    {
+     $elemMatch: { percentual: { $gte: 40 }, tipo: "sódio" },
+  },
+    { 
+      $push: 
+      { tags: "muito sódio" },
+  },
+  );
+
+  db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
