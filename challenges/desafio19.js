627f79638378d9a0946793f7e4932fc3b6b60163
samuelvanguarda <samuelvanguarda@gmail.com>
2021-09-21 18:00:31 -0300

    Requisito 20

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -1,6 +1,8 @@
 db.produtos.updateMany({},
-  { $rename: {
+  {
+    $rename: {
       descricao: "descricaoSite",
-  } });
+    },
+  });
 
 db.produtos.find({}, { _id: 0, nome: 1, descricao: 1, descricaoSite: 1 });
