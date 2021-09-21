a064a1e91f2c8f898f395e76a9ca644c2a0da070
theusseveen22 <mateus.krauss@gmail.com>
2021-09-20 21:23:26 -0300

    Adiciona todos os requisitos t1

---
+++ challenges/desafio19.js
@@ -0,0 +1,7 @@
+db.produtos.updateMany({}, 
+    {
+    $rename: { descricao: "descricaoSite" },
+});
+
+db.produtos.find({},
+    { nome: 1, descricao: 1, _id: 0, descricaoSite: 1 });
---
+++ challenges/desafio21.js
@@ -0,0 +1,5 @@
+db.produtos.find(
+    {
+      $expr: { $gt: ["$curtidas", "$vendidos"] },
+    },
+  );
