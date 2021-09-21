c8c1d0e334cb7f79dfea24acfa22c0cba5923f91
DADA <davimarque2000@gmail.com>
2021-09-21 12:16:52 -0300

    Conserta desafios 3 e 4

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,7 +1,16 @@
 db.produtos.updateMany(
     {},
-    { $set: { valorUnitario: NumberInt(0) } },
+    { $set: { valorUnitario: NumberInt("0") } },
   );
-  db.produtos.updateMany({ tags: "bovino" }, { $inc: { avaliacao: 5 } });
-  db.produtos.updateMany({ tags: "ave" }, { $inc: { avaliacao: 3 } });
-  db.produtos.find({}, { nome: 1, avaliacao: 1, _id: 0 });
+  db.produtos.updateMany(
+      { tags: "bovino" },
+       { $inc: { avaliacao: 5 } },
+       );
+  db.produtos.updateMany(
+      { tags: "ave" },
+      { $inc: { avaliacao: 3 } },
+  );
+  db.produtos.find(
+      {},
+       { nome: 1, avaliacao: 1, _id: 0 },
+       );
--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,6 +1,6 @@
 db.produtos.updateOne(
   { nome: "Big Mac" },
-  { $currentDate: { ultimaModificacao: true } },
+  { $currentDate: { ultimaModificacao: { $type: "date" } } },
 );
 db.produtos.find(
     { ultimaModificacao: { $exists: true } }, { nome: 1, ultimaModificacao: 1, _id: 0 },
