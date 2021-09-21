364e18dbd9717fd29e6e61fe1f2a8416ee93a28c
rubia emily <rubiaemilly29@gmail.com>
2021-09-21 17:38:43 -0300

    Update desafio13.js

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,6 +1,17 @@
 db.produtos.updateMany(
-  { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } } } },
-  { $push: { tags: "muito sódio" } },
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: "sódio",
+        percentual: { $gte: 40 },
+      },
+    },
+  },
+  {
+    $push: {
+      tags: "muito sódio",
+    },
+  },
 );
 
-db.produtos.find({}, { nome: 1, _id: 0, tags: 1 });
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
