35e94e6446e765eac50250b98f13f12b3c93fdeb
rubia emily <rubiaemilly29@gmail.com>
2021-09-21 17:13:04 -0300

    Update desafio10.js

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -6,3 +6,5 @@ db.produtos.updateMany(
 db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
 
 db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.6": 120 } });
+
+db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 });
