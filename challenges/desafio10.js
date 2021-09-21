b7422ed81f543bff6c4af2a522f94841696e45d7
samuelvanguarda <samuelvanguarda@gmail.com>
2021-09-21 17:09:03 -0300

    Requisito 12

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,14 +1,15 @@
 db.produtos.updateMany({},
   { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
 
-db.produtos.updateOne({ nome: "Big Mac" },
-  { $inc: { "vendasPorDia.3": 60 } });
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $inc: { "vendasPorDia.3": 60 } },
+);
 
-db.produtos.updateOne({ nome: "Big Mac" },
-  { $inc: { "vendasPorDia.3": 60 } });
+db.produtos.updateMany(
+  { tags: { $in: ["bovino", "pão"] } },
+  { $inc: { "vendasPorDia.6": 120 } },
+);
 
-db.produtos.updateMany({ tag: { $in: ["bovino", "pão"] } },
-  { $inc: { "vendasPorDia.6": 120 } });
-
-db.produtos.find({}, 
-  { _id: 0, nome: 1, vendasPorDia: 1 });
+db.produtos.find({},
+  { nome: 1, vendasPorDia: 1, _id: 0 });
