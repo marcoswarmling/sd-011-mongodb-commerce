89fda165ca49df1c187b2050dd85908323f8e3b5
Rubia <rubiaemilly29@gmail.com>
2021-09-21 02:11:45 -0300

    Ds4 e 5

---
+++ desafio5.js
@@ -0,0 +1,3 @@
+db.produtos.updateMany({ nome: { $ne: "McChicken" } }, { $addToSet: { ingredientes: "ketchup" } });
+
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { nome: 1, _id: 0 });
