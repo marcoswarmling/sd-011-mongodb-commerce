9375b241b474cc4e2b9f14f4edaca385328b9eea
limaleonardo <lima.leonardo.r@gmail.com>
2021-09-22 19:33:22 -0300

    soluciona desafio 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,20 @@
+db.produtos.updateMany(
+  { valoresNutricionais: { 
+      $elemMatch: { 
+        tipo: "sódio",
+        percentual: { $lt: 40, $gt: 20 }, 
+      }, 
+    }, 
+  },
+  { $push: { 
+      tags: {
+        $each: ["contém sódio"],
+      },
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, tags: 1 },
+);
