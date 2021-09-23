f7f864136aeb09e43160db2bddc1d9aeea0404a0
getuliogutemberg <getulio.dev@gmail.com>
2021-09-23 02:17:24 -0300

    Resolve requisito 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,15 @@
+db.produtos.updateOne(
+  { nome: "Quarteirão com Queijo" },
+  { $pop: 
+    { ingredientes: -1 },
+  },
+);
+
+db.produtos.find(
+  {},
+  { 
+    ingredientes: 1, 
+    nome: 1,
+    _id: 0,
+  },
+);
