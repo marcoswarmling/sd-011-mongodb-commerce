325fdaaa2593bc1b026a3d0987081566a8c83f89
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2021-09-23 12:24:48 -0300

    Ate req 5 resolvido

---
+++ challenges/desafio5.js
@@ -0,0 +1,8 @@
+db.produtos.updateMany({ $or: [{
+  nome: { $ne: "McChicken" } }, { ingredientes: { $ne: "ketchup" } }],
+},
+   {
+  $push: { ingredientes: { $each: ["ketchup"] } },
+});
+
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
