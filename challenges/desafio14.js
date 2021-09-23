cddf27671a279ccfb6798e7dc462b5d07c625e05
getuliogutemberg <getulio.dev@gmail.com>
2021-09-23 02:45:55 -0300

    Fix lint 14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -2,7 +2,7 @@ db.produtos.updateMany(
   { valoresNutricionais: 
     { $elemMatch: 
       { tipo: "sódio", percentual: { $gt: 20, $lt: 40 } },
-    } 
+    },
   },
   { $push: { tags: "contém sódio" } },
 );
