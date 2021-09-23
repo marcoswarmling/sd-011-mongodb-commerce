3ddc9209968a1be8df774b2c454da17b769ff118
Michelle <mls.srbek@gmail.com>
2021-09-23 12:57:44 -0300

    corrections

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,3 +1,5 @@
-db.produtos.updateMany({ valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } } } }, { $push: { tags: "muito sódio" } });
+db.produtos.updateMany({ valoresNutricionais: { $elemMatch: 
+    { tipo: "sódio", percentual: { $gte: 40 } } } }, 
+    { $push: { tags: "muito sódio" } });
 
 db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,3 +1,4 @@
-db.produtos.updateMany({ nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } }, { $push: { ingredientes: "bacon" } });
+db.produtos.updateMany({ nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
+ { $push: { ingredientes: "bacon" } });
 
 db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
