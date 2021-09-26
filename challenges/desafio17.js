aaffc09aee032908089740b4f1931f507a645c70
AlexSmith7192 <alexsmith7192@gmail.com>
2021-09-26 11:34:43 -0300

    fix challenge seventeen

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,3 +1,3 @@
-db.produtos.createIndex({ subject: "text" }, { default_language: "portuguese" });
+db.produtos.createIndex({ descricao: "text" }, { default_language: "portuguese" });
 
 db.produtos.find({ $text: { $search: "frango hamburguer" } }).count();
