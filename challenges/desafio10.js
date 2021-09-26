df2e8fe0feae305fc6985bf96a58955c6d5b204e
juliabaptista <juliabaptistadeoliveira@gmail.com>
2021-09-26 20:22:03 -0300

    Feat: req-6-to-10

---
+++ challenges/desafio10.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+  {},
+  { $push: { vendasPorDia: { $each: [0, 0, 0, 0, 0, 0, 0] } } },
+);
+
+db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
+
+db.produtos.updateMany(
+  { tags: { $all: ["bovino", "pão"] } },
+  { $inc: { "vendasPorDia.6": 120 } },
+);
+
+db.produtos.find({}, { nome: true, vendasPorDia: true, _id: false });
