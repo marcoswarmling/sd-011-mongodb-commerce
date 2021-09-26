1258d61879139b45f78905d9321260453e411b42
AlexSmith7192 <alexsmith7192@gmail.com>
2021-09-26 10:06:55 -0300

    resolves challenge eleven

---
+++ challenges/desafio11.js
@@ -0,0 +1,19 @@
+db.produtos.updateMany(
+  {},
+  {
+    $push: {
+      tags: {
+        $each: [
+          "combo",
+          "tasty",
+        ],
+        $sort: { nome: 1 },
+      },
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, tags: 1 },
+);
