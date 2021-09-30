f9cc646f198ee3f794d28e22e1bbb66773c31a20
Ana Carolina R Santos <kroline0693@gmail.com>
2021-09-29 21:52:41 -0300

    desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,25 @@
+db.produtos.updateMany(
+  {},
+  {
+    $set: { valorUnitario: NumberDecimal(0.00) },
+  },
+);
+
+db.produtos.updateMany(
+  { tags: "bovino" },
+  {
+    $inc: { avaliacao: 5 },
+  },
+);
+
+db.produtos.updateMany(
+  { tags: "ave" },
+  {
+    $inc: { avaliacao: 3 },
+  },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, avaliacao: 1, _id: 0 },
+);
