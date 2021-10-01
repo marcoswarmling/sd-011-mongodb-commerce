5b70d1a5c6ad144cf00c9bd3ae510af9549d7c03
roberto <robertonnv@gmail.com>
2021-09-30 22:04:44 -0300

    Desafio 8

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,2 +1,3 @@
-db.produtos.updateMany( { nome: { $in: ["Quarteirão com Queijo"] } }, { $pop: { ingredientes: -1 }});
-db.produtos.find( { nome: 1, ingredientes: 1, _id: 0 });
+db.produtos.updateMany({ nome: { $in: ["Quarteirão com Queijo"] } },
+{ $pop: { ingredientes: -1 } });
+db.produtos.find({ nome: 1, ingredientes: 1, _id: 0 });
