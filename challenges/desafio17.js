0afd82e266e6083a714e2ef0fd53f0d18b5456af
roberto <robertonnv@gmail.com>
2021-10-01 16:07:25 -0300

    Desafio 17

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,3 +1,4 @@
 db.produtos.createIndex({ descricao: "text" },
 { default_language: "portuguese" });
+db.produtos.find({ $text: { $search: "frango hamburguer" } }).count();
 // explicação tirada da pagina https://docs.mongodb.com/manual/tutorial/specify-language-for-text-index/
