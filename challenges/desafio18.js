e2e4844325e928e1ff6cc3742b6e126359040075
roberto <robertonnv@gmail.com>
2021-10-01 16:20:15 -0300

    Desafio 18

---
+++ challenges/desafio18.js
@@ -0,0 +1,4 @@
+db.produtos.createIndex({ descricao: "text" },
+{ default_language: "portuguese" });
+db.produtos.find({ $text: { $search: "\"feito com\"" } }).count();
+// https://app.betrybe.com/course/back-end/mongodb-updates-simples-e-complexos/updates-complexos-arrays-parte-2/4fd2d65a-b343-43a7-adb2-fc06227153f7/conteudos/180eb9df-26a4-4499-a60c-1f91d6e45f79/operador-text/eb96d105-ba91-473b-aec9-d5af0110f259?use_case=side_bar
