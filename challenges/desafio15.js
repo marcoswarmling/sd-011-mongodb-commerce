38ab5b3e006df5461dfba18dbb36d74c563dfdfe
roberto <robertonnv@gmail.com>
2021-10-01 15:53:27 -0300

    Desafio 16

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -1,1 +1,3 @@
 db.produtos.find({ nome: { $regex: /mc/i } }).count();
+
+// Explicação do course https://app.betrybe.com/course/back-end/mongodb-updates-simples-e-complexos/updates-complexos-arrays-parte-2/4fd2d65a-b343-43a7-adb2-fc06227153f7/conteudos/180eb9df-26a4-4499-a60c-1f91d6e45f79/operador-regex/52a5d466-2934-478f-b4c8-eecf1ec13388?use_case=side_bar
