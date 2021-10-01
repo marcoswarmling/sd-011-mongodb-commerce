8535de7135c1c36b9c26da2377429016716ad4b0
roberto <robertonnv@gmail.com>
2021-10-01 10:10:09 -0300

    Desafio 13

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,3 +1,4 @@
-db.produtos.updateMany({ valoresNutricionais: {$eleMatch: { tipo: "sódio", percentual: { $gte: 40 } } } },
+db.produtos.updateMany({ valoresNutricionais: { $eleMatch: { tipo: "sódio",
+percentual: { $gte: 40 } } } },
 { push: { tags: { $each: ["Muito sódio"] } } });
 db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
