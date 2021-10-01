c53159e847acbaeeec5ac789180982c3a16170f2
roberto <robertonnv@gmail.com>
2021-10-01 16:03:17 -0300

    Desafio 17

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,2 +1,3 @@
-
+db.produtos.createIndex({ descricao: "text" },
+{ default_language: "portuguese" });
 // explicação tirada da pagina https://docs.mongodb.com/manual/tutorial/specify-language-for-text-index/
