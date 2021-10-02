3b0f4229928b384ea3c06c7e4d5cdefda79597e9
Arlesson Moura <arlessonmss@gmail.com>
2021-10-02 19:22:25 -0300

    Os 6 primeiros. Mizifio criou coragem, mas agr faço pausa para Jantar

---
+++ challenges/desafio5.js
@@ -0,0 +1,6 @@
+db.produtos.updateMany({
+$and: [{ nome: { $ne: "McChicken" } }, { ingredientes: { $nin: ["ketchup"] } }],
+},
+{ $push: { ingredientes: "ketchup" } });
+
+db.produtos.find({ }, { _id: false, nome: true, ingredientes: true });
