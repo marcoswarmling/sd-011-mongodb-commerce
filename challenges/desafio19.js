56d00810332fbe787d4139c6dbc2e93e4a7967e6
pmaiamateus <pmaiamateus@gmail.com>
2021-10-04 00:03:50 -0300

    update desafio19

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -1,2 +1,3 @@
 db.produtos.updateMany({}, { $rename: { descricao: "descricaoSite" } });
 db.produtos.find({}, { nome: 1, descricao: 1, descricaoSite: 1, _id: 0 });
+// código do rename encontrado na documentação https://docs.mongodb.com/manual/reference/operator/update/rename/
