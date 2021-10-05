00cbc73533e0d95e9992cbde95575bc4896c003c
Arlesson Moura <arlessonmss@gmail.com>
2021-10-05 08:10:52 -0300

    dar commits ate o Evaluator parar de me engabelar requisitos

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,3 +1,5 @@
 db.produtos.updateMany({ }, { $set: { criadoPor: "Ronald McDonald" } });
 
 db.produtos.find({ }, { _id: false, nome: true, criadoPor: true });
+
+// Dar commits até o Evaluator parar de engabelar requisitos.
