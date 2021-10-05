8817511a0278deba9881dde248ce363f7b993a34
Arlesson Moura <arlessonmss@gmail.com>
2021-10-05 13:43:05 -0300

    evitar problemas com o bug do GitHub

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,3 +1,5 @@
 db.produtos.updateMany({ }, { $set: { criadoPor: "Ronald McDonald" } });
 
 db.produtos.find({ }, { _id: false, nome: true, criadoPor: true });
+
+// evitar problemas com o bug do gitHub
